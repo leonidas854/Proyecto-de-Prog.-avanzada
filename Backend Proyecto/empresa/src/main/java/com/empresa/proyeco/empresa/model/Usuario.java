e07b5b9619c5a54efbb3ae8e7d021e822cb78046
Thiago <tsosachugar@gmial.com>
2024-11-12 12:53:12 -0400

    documentos

--- Backend Proyecto/empresa/src/main/java/com/empresa/proyeco/empresa/model/Usuario.java
+++ Backend Proyecto/empresa/src/main/java/com/empresa/proyeco/empresa/model/Usuario.java
@@ -22,7 +22,7 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private int id;
+    private Long id;
 
     @Column(nullable = false)
     private String nombre;
